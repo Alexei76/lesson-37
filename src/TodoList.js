d4fb8b9ef27fa88e3bfcbfb37c4fb83d6fd0a8f6
alexei <webdesfreelance@gmail.com>
2020-07-14 18:19:38 +0300

    save

---
+++ src/TodoList.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+export function TodoList(props){
+return(
+
+ <ul className="todolist" id="todolist">
+            {this.state.todos.length ? (
+              this.state.todos.map((item) => {
+                return (<li>
+                  <input type="checkbox" /> 
+                  <span className={item.status ? 'complete' : '' }>{item.text}</span>
+                </li>)
+              })
+            ) : null}
+          </ul>
+
+)
+
+
+
+}
