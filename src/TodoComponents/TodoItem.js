7757926a6eb802fd2bec091966a50e931e5aaebf
alexei <webdesfreelance@gmail.com>
2020-07-16 18:04:50 +0300

    remove

--- src/TodoComponents/TodoItem.js
+++ src/TodoComponents/TodoItem.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Button } from '../uielements/Button';
 
 export function TodoItem(props){
     return(
         <li className="todolist__item"><input type="checkbox" /> 
         <span className={props.item.status ? 'complete' : '' }>
-            {props.item.text}</span></li>
+            {props.item.text}</span>
+            <Button>Remove</Button>
+            </li>
     )
 
 }
