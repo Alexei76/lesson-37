25b8b1da645836d76f00ba5cde54f16a289ea0f8
alexei <webdesfreelance@gmail.com>
2020-07-16 18:26:56 +0300

    save

--- src/TodoComponents/TodoItem.js
+++ src/TodoComponents/TodoItem.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Button } from '../uielements/Button';
+import { TodoListItem } from '../uielements/TodoListItem';
 
 export function TodoItem(props){
     return(
-        <li className="todolist__item"><input type="checkbox" /> 
+        <TodoListItem><input type="checkbox" /> 
         <span className={props.item.status ? 'complete' : '' }>
             {props.item.text}</span>
             <Button>Remove</Button>
-            </li>
+            </TodoListItem>
     )
 
 }
---
+++ src/uielements/TodoListItem.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+
+export const TodoListItem = styled.li`
+padding: 15px 9px;
+border-radius: 2px;
+border: 1px solid grey;
+width: 425px;
+background-color: #eaeaea;
+margin-bottom: 3px;
+
+`
