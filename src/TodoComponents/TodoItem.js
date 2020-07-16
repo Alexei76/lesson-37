5bee3bce6f8f5dc69676f83cac820ecc0887682f
alexei <webdesfreelance@gmail.com>
2020-07-16 17:15:21 +0300

    save

--- src/TodoComponents/TodoItem.js
+++ src/TodoComponents/TodoItem.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 export function TodoItem(props){
     return(
-        <li><input type="checkbox" /> <span className={props.item.status ? 'complete' : '' }>{props.item.text}</span></li>
+        <li className="todolist__item"><input type="checkbox" /> 
+        <span className={props.item.status ? 'complete' : '' }>
+            {props.item.text}</span></li>
     )
 
 }
